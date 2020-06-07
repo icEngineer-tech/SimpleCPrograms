0dc0236b54cde17428a1ff6d64fad00b085294b6
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-06-07 19:00:50 +0100

    Create concate2strings_with_pointer.c

---
+++ concate2strings_with_pointer.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char* conct(const char* s, const char* s1)
+{
+	char* res = (char*)malloc(len(s) + len(s1) + 1);
+	int i = 0;
+	while (*s)
+	{
+		*(res+i) = *s;
+		s++;
+		i++;
+	}
+	while (*s1)
+	{
+		*(res+i) = *s1;
+		s1++;
+		i++;
+	}
+	*(res+i) = '\0';
+	return res;
+}
+void delete_char(char* dc)
+{
+	free(dc);
+}
+int main()
+{
+	const char* str = "hello";      // see, I'm using pointer to char because I allocated a new mem to an other string inside the function
+	const char* str2 = " world";
+	char* res = conct(str, str2);
+	printf("%s", res);
+	delete_char(res);
+	/*char* rs = reverse(str);
+	printf("%s", rs);
+	delete_char(rs);*/
+	return 0;
+}
