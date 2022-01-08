67864781df8de0630ed0a6e62c90edf350a84bfd
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2022-01-08 19:48:24 +0100

    Update swap_2_bits.c

--- swap_2_bits.c
+++ swap_2_bits.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <math.h>
-//write a program that swaps the 3rd and 4th bit
+
 int swap(int n, unsigned bit1, unsigned bit2)
 {
     return ((int)pow(2, bit1-1) & n)<<abs(bit2-bit1) | ((int)pow(2, bit2-1) & n) >>abs(bit2-bit1) | ((0xffffffff- (int)pow(2, bit1-1) - (int)pow(2, bit2-1)) & n);
 }
 
-int main() {
-    // Write C code here
+int main() 
+{
     printf("%d", swap(10, 1, 4));
-    
     return 0;
 }
